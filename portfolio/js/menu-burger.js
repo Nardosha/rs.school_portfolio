ee2d84963a3497f37a428c79a2612ca10eef9725
Natalia Degtyareva <lalabumlala@gmail.com>
2022-01-23 23:19:16 +0300

    feat: add burger-menu

---
+++ portfolio/js/menu-burger.js
@@ -0,0 +1,21 @@
+const menuBurger = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.menu__body')
+
+
+if (menuBurger) {
+	menuBurger.addEventListener('click', function(e) {
+		document.body.classList.toggle('_lock')
+		menuBurger.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+	})
+	menuBody.addEventListener('click', closeMenu)
+}
+
+
+function closeMenu() {
+	if (menuBurger.classList.contains('_active')) {
+		document.body.classList.remove('_lock')
+		menuBurger.classList.remove('_active')
+		menuBody.classList.remove('_active')
+	}
+}
